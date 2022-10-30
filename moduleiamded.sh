c4958ffb2df76b877858dc0283aaf2f2d208f511
iamded <dedetmasri@gmail.com>
2022-10-30 20:19:14 +0700

    Create moduleiamded.sh

---
+++ moduleiamded.sh
@@ -0,0 +1,35 @@
+echo "Dont forget subcribe my chanel: iam ded !!!..."
+sleep 3
+termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
+clear
+sleep 3
+echo "Checking package harap menunggu ya bos..!!!"
+sleep 3
+pkg install python bash git -y
+pkg install python2 -y
+pkg install figlet -y
+pkg install wget -y
+pkg install curl -y
+pkg install ruby -y
+pkg install php -y
+pkg install root-repo -y
+pkg install tsu -y
+pkg install zlib -y
+pkg install openssl -y
+pkg install nodejs -y
+gem install lolcat -y
+pkg install bastet &> //dev/null
+pkg install vitetris &> //dev/null
+pkg install nsnake &> //dev/null
+pkg install moon-buggy &> //dev/null
+pkg install ninvaders &> //dev/null
+echo "Package Already Installed"
+pip install requests colorama bs4 phonenumbers
+pip install colorama bs4 requests pyfiglet tqdm
+pip2 install colorama bs4 requests pyfiglet tqdm
+echo "Pip Already Installed"
+pkg update -y
+pkg upgrade -y
+echo "Termux terupdate terima kasih"
+clear
+sleep 3
