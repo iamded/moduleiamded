9091048f15e13d8c310343859e4498157a69c62a
iamded <dedetmasri@gmail.com>
2022-11-01 12:15:48 +0700

    Update moduleiamded.sh

--- moduleiamded.sh
+++ moduleiamded.sh
@@ -6,24 +6,25 @@ sleep 5
 echo "Checking package harap menunggu ya bos..!!!"
 sleep 3
 termux-setup-storage
-pkg install python bash git -y
-pkg install python2 -y
-apt install root-repo -y
-apt install x11-repo -y
-apt install termux-api -y
-apt install git -y
-apt install wget -y
-aot install curl -y
-apt install python -y
-apt install php -y
-apt install ruby -y
-apt install mc -y
-apt install zsh -y
-apt install toilet -y
-apt install figlet -y
-apt install cloudflared -y
-apt install openssh -y
-apt install sshpass -y
+pkg update -y
+pkg upgrade -y
+pkg install bash git -y
+pkg install root-repo -y
+pkg install x11-repo -y
+pkg install termux-api -y
+pkg install git -y
+pkg install wget -y
+pkg install curl -y
+pkg install python -y
+pkg install php -y
+pkg install ruby -y
+pkg install mc -y
+pkg install zsh -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install cloudflared -y
+pkg install openssh -y
+pkg install sshpass -y
 pkg install tsu -y
 pkg install zlib -y
 pkg install openssl -y
@@ -41,8 +42,11 @@ pip2 install colorama bs4 requests pyfiglet tqdm
 pip3 install colorama bs4 requests pyfiglet tqdm
 python3 -m pip install --upgrade pip
 echo "Pip Already Installed"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/iamded/hacktr/main/tool.py)"
-clear
 echo "Your Termux is ready...!!!"
+echo "Install Tool Hack..waiting!!!"
+clear
+git clone https://github.com/iamded/hacktr
+cd hacktr
 clear
 sleep 3
+python2 tool.py
