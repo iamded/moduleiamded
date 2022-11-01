e7d844d6073478a6b417d0dfb755b9f00f380ab0
iamded <dedetmasri@gmail.com>
2022-11-01 11:36:02 +0700

    Update moduleiamded.sh

--- moduleiamded.sh
+++ moduleiamded.sh
@@ -2,23 +2,33 @@ echo "Dont forget subcribe my chanel: iam ded !!!..."
 sleep 3
 termux-open-url https://youtube.com/channel/UCk0IHP3kOrqN86sXjs1c_Xw
 clear
-sleep 3
+sleep 5
 echo "Checking package harap menunggu ya bos..!!!"
 sleep 3
+termux-setup-storage
 pkg install python bash git -y
 pkg install python2 -y
-pkg install figlet -y
-pkg install wget -y
-pkg install curl -y
-pkg install ruby -y
-pkg install php -y
-pkg install unzip -y
-pkg install root-repo -y
+apt install root-repo -y
+apt install x11-repo -y
+apt install termux-api -y
+apt install git -y
+apt install wget -y
+aot install curl -y
+apt install python -y
+apt install php -y
+apt install ruby -y
+apt install mc -y
+apt install zsh -y
+apt install toilet -y
+apt install figlet -y
+apt install cloudflared -y
+apt install openssh -y
+apt install sshpass -y
 pkg install tsu -y
 pkg install zlib -y
 pkg install openssl -y
 pkg install nodejs -y
-gem install lolcat 
+gem install lolcat  
 pkg install bastet &> //dev/null
 pkg install vitetris &> //dev/null
 pkg install nsnake &> //dev/null
@@ -28,9 +38,11 @@ echo "Package Already Installed"
 pip install requests colorama bs4 phonenumbers
 pip install colorama bs4 requests pyfiglet tqdm
 pip2 install colorama bs4 requests pyfiglet tqdm
+pip3 install colorama bs4 requests pyfiglet tqdm
+python3 -m pip install --upgrade pip
 echo "Pip Already Installed"
-pkg update -y
-pkg upgrade -y
-echo "Termux terupdate terima kasih"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+clear
+echo "Your Termux is ready...!!!"
 clear
 sleep 3
